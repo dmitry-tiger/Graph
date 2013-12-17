6510cc3e73a993bc851e444c0be1bed262e8ff58
Dmitry Tigrov <omo@bk.ru>
2013-12-18 01:55:11 +0400

    add new routes

--- lib/Graph.pm
+++ lib/Graph.pm
@@ -4,15 +4,22 @@ use Mojo::Base 'Mojolicious';
 # This method will run once at server start
 sub startup {
   my $self = shift;
-
+  $self->setup_routing;
   # Documentation browser under "/perldoc"
   $self->plugin('PODRenderer');
 
   # Router
-  my $r = $self->routes;
+
 
   # Normal route to controller
-  $r->get('/')->to('example#welcome');
+}
+
+sub setup_routing {
+    my $self = shift;
+    my $r = $self->routes;
+      $r->get('/')->to('screen#new');
+      $r->get('/dash/')->to('screen#dash');
+      $r->get('/:id/:type', name => [qw(view dash)])->to('screen#view');
 }
 
 1;
